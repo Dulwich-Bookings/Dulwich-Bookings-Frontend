7fe679efe08a24c99be231a93c680d3da20e7940
ShenyiCui <shenyicui@outlook.com>
2022-05-13 00:22:41 +0800

    Init Project

---
+++ src/tailwind.config.js
@@ -0,0 +1,24 @@
+const TailWindTheme = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  important: true,
+  theme: {
+    extend: {},
+    fontFamily: {},
+    colors: {},
+    fontSize: {
+      sm: 12,
+      md: 14,
+      lg: 19,
+      xl: 20,
+      xxl: 24,
+    },
+  },
+  variants: {
+    extend: {
+      dropShadow: ['hover'],
+    },
+  },
+};
+
+module.exports = TailWindTheme;
