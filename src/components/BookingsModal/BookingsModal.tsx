13937e90c6a3b0c879e70fd3872439b4935fe46e
ShenyiCui <shenyicui@outlook.com>
2022-06-08 23:22:20 +0800

    refactor: add dummy data to modal

--- src/components/BookingsModal/BookingsModal.tsx
+++ src/components/BookingsModal/BookingsModal.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import Calendar from '@components/Calendar/Calendar';
 import BookingsHeader from '@components/BookingsModal/BookingsHeader.tsx/BookingsHeader';
+import { dummyCalendarData, dummyResourceData } from '@/consts/dummyData';
 import { Stack, Modal, Box, ThemeProvider, createTheme } from '@mui/material';
 
 const theme = createTheme({
@@ -27,9 +28,9 @@ const BookingsModal = ({ openState, handleCloseModal }: Props) => {
       <Modal className='flex justify-center items-center' open={openState} onClose={handleCloseModal}>
         <Box className='laptop:w-11/12 laptop:h-5/6 laptop:mt-12 laptop:rounded-lg w-full h-full px-14 pt-10 bg-white'>
           <CloseIcon onClick={handleCloseModal} className='float-right cursor-pointer hover:text-grayAccent' />
-          <Stack className='h-full' spacing={{ xs: 1, md: -7 }}>
-            <BookingsHeader id={1} title={'Tech 4'} description={'this is a description'} />
-            <Calendar />
+          <Stack className='h-full' spacing={{ xs: 1, md: -6 }}>
+            <BookingsHeader id={1} title={dummyResourceData.title} description={dummyResourceData.description} />
+            <Calendar data={dummyCalendarData} />
           </Stack>
         </Box>
       </Modal>
