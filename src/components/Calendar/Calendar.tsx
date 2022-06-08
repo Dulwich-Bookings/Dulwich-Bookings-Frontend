f28005647b658225736d52065bd36fedc61bf010
ShenyiCui <shenyicui@outlook.com>
2022-06-08 23:22:01 +0800

    feature: add date click callback

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -4,51 +4,34 @@ import { useTheme } from '@mui/material/styles';
 import { Box } from '@mui/material';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 import DayHeaderContent from '@components/Calendar/DayHeaderContent/DayHeaderContent';
 import SlotLabelContent from '@components/Calendar/SlotLabelContent/SlotLabelContent';
 import './Calendar.css';
 
-const dummyData = [
-  {
-    title: 'Form',
-    start: '2022-06-06T08:15:00',
-    end: '2022-06-06T08:30:00',
-    description: 'Lecture',
-    backgroundColor: '#D9D9D9',
-    borderColor: '#D9D9D9',
-    textColor: 'black',
-  },
-  {
-    title: 'Lesson [Period 1]',
-    start: '2022-06-06T08:35:00',
-    end: '2022-06-06T09:30:00',
-    description: 'Lecture',
-    backgroundColor: '#D9D9D9',
-    borderColor: '#D9D9D9',
-    textColor: 'black',
-    editable: true,
-  },
-  {
-    title: 'Lesson [Period 2]',
-    start: '2022-06-06T09:35:00',
-    end: '2022-06-06T10:30:00',
-    description: 'Lecture',
-    backgroundColor: '#D9D9D9',
-    borderColor: '#D9D9D9',
-    textColor: 'black',
-  },
-  {
-    title: 'MIT Meeting',
-    start: '2022-06-06T10:30:00',
-    end: '2022-06-06T11:00:00',
-    description: 'Meeting',
-  },
-];
+export type EventData = {
+  title: string;
+  start: string;
+  end: string;
+  description: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  editable: boolean;
+};
 
-const Calendar = () => {
+type Props = {
+  data: EventData[];
+};
+
+const Calendar = ({ data }: Props) => {
   const theme = useTheme();
   const isMobile = !useMediaQuery(theme.breakpoints.up('sm'));
+
+  const handleDateClick = (e: DateClickArg) => {
+    console.log(e.dateStr);
+  };
+
   return (
     <Box className='h-full'>
       <FullCalendar
@@ -71,7 +54,9 @@ const Calendar = () => {
         slotEventOverlap={false}
         plugins={[timeGridPlugin, interactionPlugin]}
         initialView={isMobile ? 'timeGridDay' : 'timeGridWeek'}
-        events={dummyData}
+        dateClick={handleDateClick}
+        editable={true}
+        events={data}
       />
     </Box>
   );
