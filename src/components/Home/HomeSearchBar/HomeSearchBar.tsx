62c1ad57ec49e2cebe83dbd75ed5b689fc10c9fd
Brian Q <astraxquek@gmail.com>
2022-06-10 01:02:27 +0800

    add menu/button into search bar - added drop-down menu - search button

--- src/components/Home/HomeSearchBar/HomeSearchBar.tsx
+++ src/components/Home/HomeSearchBar/HomeSearchBar.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Grid, OutlinedInput, FormControl } from '@mui/material';
+import { Grid, OutlinedInput, FormControl, InputAdornment, Button } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import HomeMenu from './HomeMenu';
 
 const HomeSearchBar = () => {
   return (
     <Grid container justifyContent='center'>
       <FormControl className='rounded-full w-3/4 bg-bgWhite' sx={{ boxShadow: 12 }}>
-        <OutlinedInput id='search-rooms' />
+        <OutlinedInput
+          id='search-rooms'
+          startAdornment={
+            <InputAdornment position='start'>
+              <HomeMenu />
+            </InputAdornment>
+          }
+          endAdornment={
+            <InputAdornment position='end'>
+              <Button>
+                <Search />
+              </Button>
+            </InputAdornment>
+          }
+        />
       </FormControl>
     </Grid>
   );
