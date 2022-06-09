fda8511616db70a108370106a3f273b8b130fdb3
ShenyiCui <shenyicui@outlook.com>
2022-06-10 01:33:16 +0800

    add login form validation logic

--- src/components/Landing/Forms/LoginForm/LoginForm.tsx
+++ src/components/Landing/Forms/LoginForm/LoginForm.tsx
@@ -11,11 +11,14 @@ import { updateCurrentUser } from '@/modules/user/userSlice';
 import AuthService from '@/api/auth/AuthService';
 import UserService from '@/api/user/UserService';
 import { useApi } from '@/api/ApiHandler';
+import { InputValidation } from '@components/InputWithLabel/InputWithLabel';
 
 const LoginForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [emailError, setEmailError] = useState<InputValidation>({ isError: false, errorHelperText: '' });
+  const [passwordError, setPasswordError] = useState<InputValidation>({ isError: false, errorHelperText: '' });
   const dispatch = useDispatch();
   const history = useHistory();
   const [login] = useApi(() => AuthService.login(email, password), true, true, false);
@@ -31,9 +34,30 @@ const LoginForm = () => {
     routeText: 'Forgot your password',
   };
 
+  const formValidation = () => {
+    const errorText = 'Field Cannot be Empty';
+    const errorObj: InputValidation = {
+      isError: true,
+      errorHelperText: errorText,
+    };
+    const noErrorObj: InputValidation = {
+      isError: false,
+      errorHelperText: '',
+    };
+    const isValidEmail = email.length !== 0;
+    const isValidPassword = password.length !== 0;
+    setEmailError(isValidEmail ? noErrorObj : errorObj);
+    setPasswordError(isValidPassword ? noErrorObj : errorObj);
+
+    if (!isValidEmail || !isValidPassword) {
+      throw new Error('Form Invalid');
+    }
+  };
+
   const handleSignIn = async () => {
     try {
       setIsLoading(true);
+      formValidation();
       await login();
       const res = await getSelf();
       if (res.isSuccess) {
@@ -58,6 +82,7 @@ const LoginForm = () => {
           labelText='Email'
           inputPlaceholder='name@dulwich.org'
           inputType='text'
+          inputValidation={emailError}
         />
         <InputWithLabel
           inputHandleOnChange={input => setPassword(input.target.value)}
@@ -65,6 +90,7 @@ const LoginForm = () => {
           labelText='Password'
           inputPlaceholder='6+ characters, 1 special character'
           inputType='password'
+          inputValidation={passwordError}
         />
       </Stack>
       <LandingFormFooter handleOnClick={handleSignIn} buttonText='Sign in' footerLink={forgetPasswordRoute} loading={isLoading} />
