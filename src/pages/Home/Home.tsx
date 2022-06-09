78bba98162e2126038305d3c972d7766af8037d6
Brian Q <astraxquek@gmail.com>
2022-06-10 00:57:00 +0800

    fix deployment preview

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import { Stack } from '@mui/material';
 import HomeHeader from '@/components/Home/HomeHeader/HomeHeader';
 import HomeBanner from '@/components/Home/HomeBanner/HomeBanner';
 import HomeRoomList from '@/components/Home/HomeRooms/HomeRoomList';
-import HomeSearchBar from '@/components/Home/HomeSearchForm/HomeSearchBar';
+import HomeSearchBar from '@/components/Home/HomeSearchBar/HomeSearchBar';
 import Footer from '@/components/Footer/Footer';
 
 const Home = () => {
