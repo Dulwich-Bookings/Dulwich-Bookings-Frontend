168cfe17ecb5157e7e1f5d84f168324a41b776db
ShenyiCui <shenyicui@outlook.com>
2022-06-26 16:03:20 +0800

    add new type DateTime
    - create a new type called DateTime to handle conversions

---
+++ src/modules/DateTime/DateTime.ts
@@ -0,0 +1,34 @@
+import { Timezone } from '@/modules/DateTime/types';
+import moment, { Moment } from 'moment-timezone';
+
+class DateTime {
+  private utc: Moment;
+  private timezone: Timezone;
+
+  public constructor(utc: string, timezone: Timezone) {
+    DateTime.validateUTCString(utc);
+    this.utc = moment.utc(utc);
+    this.timezone = timezone;
+  }
+
+  public toLocalDate(): Moment {
+    return this.utc.tz(this.timezone);
+  }
+
+  public toTimezoneDate(timezone: Timezone): Moment {
+    return this.utc.tz(timezone);
+  }
+
+  public toUTCDate(): Moment {
+    return this.utc;
+  }
+
+  private static validateUTCString(utc: string): void {
+    const dateParsed = new Date(Date.parse(utc));
+    if (dateParsed.toISOString() !== utc || dateParsed.toUTCString() !== new Date(utc).toUTCString()) {
+      throw new Error('Invalid String format for UTC');
+    }
+  }
+}
+
+export default DateTime;
