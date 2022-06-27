affa18e5dea45bee1bf2f2d08a3142063282d4fb
ShenyiCui <shenyicui@outlook.com>
2022-06-27 21:34:17 +0800

    update school data type

--- src/modules/school/types.ts
+++ src/modules/school/types.ts
@@ -2,11 +2,13 @@ import { Timezone } from '@/modules/DateTime/types';
 export interface SchoolData {
   id: number;
   name: string;
+  alternativeName?: string;
   timezone: Timezone;
 }
 
 export interface CreateSchoolData {
   name: string;
+  alternativeName?: string;
   timezone: Timezone;
 }
 
