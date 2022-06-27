7baee8d2a3e797a512bc9125fe3b525b0e1f3113
ShenyiCui <shenyicui@outlook.com>
2022-06-27 18:01:56 +0800

    refactor add resos btn into component

--- src/components/Home/HomeHeader/HomeHeader.tsx
+++ src/components/Home/HomeHeader/HomeHeader.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { AppBar, Button, Grid } from '@mui/material';
-import { Add } from '@mui/icons-material';
+import { AppBar, Grid } from '@mui/material';
 import SchoolLogo from '@components/Home/HomeHeader/SchoolLogo/SchoolLogo';
 import UserProfileCircle from '@components/Home/HomeHeader/UserProfileCircle/UserProfileCircle';
+import AddResourceButton from '@components/Home/HomeHeader/AddResourceButton/AddResourceButton';
 
 import { useSelector } from 'react-redux';
 import { getCurrentUser } from '@/modules/user/userSlice';
 
 const HomeHeader = () => {
   const currentUser = useSelector(getCurrentUser);
+
+  const handleAddResource = () => {
+    console.log('add resource');
+  };
 
   return (
     <AppBar elevation={0} className='bg-bgWhite' position='relative'>
@@ -17,10 +21,7 @@ const HomeHeader = () => {
           <SchoolLogo />
         </Grid>
         <Grid item className='flex items-center'>
-          <Button className='bg-dulwichRed normal-case rounded-lg mr-7 mt-2.5' variant='contained' size='small' disableElevation>
-            <Add className='stroke-0 stroke-black' />
-            <p className='font-Inter text-md tracking-tight py-0.5 px-1'>Resource</p>
-          </Button>
+          <AddResourceButton handleOnClick={handleAddResource} />
           <UserProfileCircle email={currentUser?.email} className='mr-8' />
         </Grid>
       </Grid>
