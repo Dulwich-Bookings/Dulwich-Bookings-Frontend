ba22a3821f9e592176328f565f310bbc4a054de6
ShenyiCui <shenyicui@outlook.com>
2022-06-27 18:05:32 +0800

    fix initials uppercase bug

--- src/components/Home/HomeHeader/UserProfileCircle/UserProfileCircle.tsx
+++ src/components/Home/HomeHeader/UserProfileCircle/UserProfileCircle.tsx
@@ -11,7 +11,7 @@ const UserProfileCircle = ({ email, className }: Props) => {
   const initials =
     profileName.lastIndexOf('.') === -1
       ? profileName.charAt(0).toUpperCase()
-      : profileName.charAt(0) + profileName.charAt(profileName.lastIndexOf('.') + 1).toUpperCase();
+      : (profileName.charAt(0) + profileName.charAt(profileName.lastIndexOf('.') + 1)).toUpperCase();
 
   return (
     <Box className={`flex bg-bgPurple rounded-full justify-center items-center w-12 h-12 cursor-pointer ${className}`}>
