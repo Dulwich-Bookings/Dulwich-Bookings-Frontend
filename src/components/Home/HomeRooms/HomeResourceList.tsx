efb9c57f45f80c5329679e6cf759705ffa329ef7
Brian Q <astraxquek@gmail.com>
2022-06-30 22:25:50 +0800

    add placeholder for no search result

--- src/components/Home/HomeRooms/HomeResourceList.tsx
+++ src/components/Home/HomeRooms/HomeResourceList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Box, Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
 import HomeRoomItem from './HomeResourceContainer';
 import Room from '@/models/room';
 
@@ -8,13 +8,33 @@ type Props = {
 };
 
 const HomeRoomList = (props: Props) => {
+  const [isResourceEmpty, setIsResourceEmpty] = useState(false);
+  useEffect(() => {
+    if (props.rooms.length == 0) {
+      setIsResourceEmpty(true);
+      console.log('No Rooms');
+    } else {
+      setIsResourceEmpty(false);
+    }
+
+    console.log('Rooms Found');
+  }, [props.rooms]);
+
   return (
     <Box className='py-[80px]'>
-      <Grid item container spacing={3.5}>
-        {props.rooms.map(room => (
-          <HomeRoomItem key={room.id} id={room.id} roomName={room.roomName} vacancy={room.vacancy} bookmark={room.bookmark} />
-        ))}
-      </Grid>
+      {!isResourceEmpty && (
+        <Grid item container spacing={3.5}>
+          {props.rooms.map(room => (
+            <HomeRoomItem key={room.id} id={room.id} roomName={room.roomName} vacancy={room.vacancy} bookmark={room.bookmark} />
+          ))}
+        </Grid>
+      )}
+
+      {isResourceEmpty && (
+        <Typography className='font-Inter text-bgNoHover' variant='h5' textTransform='capitalize'>
+          No Resources Found.
+        </Typography>
+      )}
     </Box>
   );
 };
