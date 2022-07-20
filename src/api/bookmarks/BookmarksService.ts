f8e90721538d8dc1b337128733531e969775ea9f
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2022-07-20 21:57:53 +0800

    Create BookmarksService.ts

---
+++ src/api/bookmarks/BookmarksService.ts
@@ -0,0 +1,87 @@
+import { ResourceMappings } from '@/modules/resource/types';
+import ApiService, { ApiData } from '../ApiService';
+
+export default class BookmarksService {
+  private static getBookmarksUrl() {
+    return 'bookmark';
+  }
+
+  public static async createBookmark(createBookmarkData: ResourceMappings): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getBookmarksUrl()}/`,
+          method: 'POST',
+          data: {
+            ...createBookmarkData,
+          },
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getAllBookmarks(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: this.getBookmarksUrl(),
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getBookmarkById(id: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getBookmarksUrl()}/${id}`,
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getSelf(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getBookmarksUrl()}/getSelf`,
+          method: 'GET',
+        },
+        true,
+      );
+
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async deleteBookmarkById(id: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getBookmarksUrl()}/${id}`,
+          method: 'DELETE',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
