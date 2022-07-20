a8ea15a934d7f4a59742054f92cfda0896518bc5
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2022-07-20 21:51:46 +0800

    Create RecentlyVisitedService.ts

---
+++ src/api/recently visited/RecentlyVisitedService.ts
@@ -0,0 +1,87 @@
+import { ResourceMappings } from '@/modules/resource/types';
+import ApiService, { ApiData } from '../ApiService';
+
+export default class RecentlyVisitedService {
+  private static getRecentlyVisitedUrl() {
+    return 'recentlyVisited';
+  }
+
+  public static async createRecentlyVisited(createRecentlyVisitedData: ResourceMappings): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getRecentlyVisitedUrl()}/`,
+          method: 'POST',
+          data: {
+            ...createRecentlyVisitedData,
+          },
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getAllRecentlyVisited(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: this.getRecentlyVisitedUrl(),
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getRecentlyVisitedById(id: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getRecentlyVisitedUrl()}/${id}`,
+          method: 'GET',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async getSelf(): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getRecentlyVisitedUrl()}/getSelf`,
+          method: 'GET',
+        },
+        true,
+      );
+
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  public static async deleteRecentlyVisitedById(id: number): Promise<ApiData> {
+    try {
+      const response = await ApiService.request(
+        {
+          url: `${this.getRecentlyVisitedUrl()}/${id}`,
+          method: 'DELETE',
+        },
+        true,
+      );
+      return response;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+}
