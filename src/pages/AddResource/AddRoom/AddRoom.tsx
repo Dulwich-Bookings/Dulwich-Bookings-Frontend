137b384d371dbebb55d8089511b4038a44f00849
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2022-07-20 20:59:30 +0800

    Hide Not Mobile View

--- src/pages/AddResource/AddRoom/AddRoom.tsx
+++ src/pages/AddResource/AddRoom/AddRoom.tsx
@@ -47,7 +47,7 @@ const AddRoom = () => {
             <div className='hidden addRoomLaptop:block'>
               <AddRoomForm tagData={tags} userData={users} />
             </div>
-            <div className='flex w-screen h-screen justify-center items-center mx-5'>
+            <div className='flex w-screen h-screen justify-center items-center mx-5 addRoomLaptop:hidden'>
               <div className='font-Inter text-[24px]'>This page is not supported for mobile devices.</div>
             </div>
           </main>
