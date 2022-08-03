64afa34ccf14d2f0bae0f03f79c5dff16b0cb4f7
Brian Quek <astraxquek@gmail.com>
2022-08-03 21:24:42 +0800

    create subscription modal

--- src/components/Home/HomeResources/HomeResourceContainer/HomeResourceContainer.tsx
+++ src/components/Home/HomeResources/HomeResourceContainer/HomeResourceContainer.tsx
@@ -4,12 +4,13 @@ import { Grid, Card, CardContent, Typography, Stack } from '@mui/material';
 
 import { Bookmark, PersonOutlineOutlined, Circle } from '@mui/icons-material';
 import { ResourceData } from '@/modules/resource/types';
-import { SearchState, resourceTypes } from '@/consts/constants';
+import { SearchState, resourceTypes, searchStateMap } from '@/consts/constants';
 import { TagData } from '@/modules/tag/types';
 import ResourceTag from '@/components/Home/HomeResources/HomeResourceContainer/ResourceTag/ResourceTag';
 import ResourceRights from '@/components/Home/HomeResources/HomeResourceContainer/ResourceRights/ResourceRights';
 import { SubscriptionData } from '@/modules/subscription/types';
 import { TagMapData } from '@/modules/tagMap/types';
+import SubscriptionModal from '../SubscriptionModal/SubscriptionModal';
 
 type Props = {
   data: ResourceData | SubscriptionData;
@@ -25,6 +26,7 @@ const vacancy = true;
 
 const HomeRoomItem = (props: Props) => {
   const [isBookmark, setIsBookmark] = useState(props.isBookmark);
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   const filterTagMaps = (): TagMapData[] => {
     if (props.data.type === resourceTypes.RESOURCE) {
@@ -40,56 +42,72 @@ const HomeRoomItem = (props: Props) => {
       .includes(tag.id),
   );
 
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <Grid item className='w-full homeLaptop:w-auto'>
-      <Card className='bg-bgGray rounded-xl w-full homeLaptop:w-80 h-48 hover:shadow-[0_4px_30px_0px_rgba(0,0,0,0.25)] cursor-pointer'>
-        <div
-          className='w-full h-full'
-          onClick={() => {
-            props.onRecentlyVisitedHandler(props.data.id, props.data.type, props.isRecentlyVisited);
-          }}
+    <>
+      <Grid item className='w-full homeLaptop:w-auto'>
+        <Card
+          className='bg-bgGray rounded-xl w-full homeLaptop:w-80 h-48 hover:shadow-[0_4px_30px_0px_rgba(0,0,0,0.25)] cursor-pointer'
+          onClick={handleOpenModal}
         >
-          <CardContent className='grow'>
-            <Stack spacing={-2}>
-              <div className='w-full z-10'>
-                <Bookmark
-                  onClick={async () => {
-                    setIsBookmark(!props.isBookmark);
-                    await props.onBookmarkChangeHandler(props.data.id, props.data.type);
-                  }}
-                  className='float-right text-3xl'
-                  sx={{ color: `${isBookmark ? '#000000' : '#D9D9D9'}` }}
-                />
-              </div>
-              <Stack spacing={0.5} className='z-0'>
-                <Stack direction='row' spacing={1.5} alignItems='center'>
-                  <Circle className={`text-sm`} sx={{ color: `${vacancy ? '#76D674' : '#E25454'}` }} />
-                  <Typography gutterBottom variant='h5' component='h2' className='font-Inter'>
-                    {props.data.name}
-                  </Typography>
-                </Stack>
-                <Stack direction='row' spacing={1.5} alignItems='center'>
-                  <PersonOutlineOutlined className='text-xl text-bgNoHover' />
-                  <Stack spacing={-0.5}>
-                    <Typography className='font-Inter text-bgNoHover'>Access available to:</Typography>
-                    <Typography className='font-Inter text-bgNoHover'>
-                      {props.data.accessRights.map(role => (
-                        <ResourceRights key={role} role={role} initialIndex={props.data.accessRights[0]} />
-                      ))}
+          <div
+            className='w-full h-full'
+            onClick={() => {
+              props.onRecentlyVisitedHandler(props.data.id, props.data.type, props.isRecentlyVisited);
+            }}
+          >
+            <CardContent className='grow'>
+              <Stack spacing={-2}>
+                <div className='w-full z-10'>
+                  <Bookmark
+                    onClick={async () => {
+                      setIsBookmark(!props.isBookmark);
+                      await props.onBookmarkChangeHandler(props.data.id, props.data.type);
+                    }}
+                    className='float-right text-3xl'
+                    sx={{ color: `${isBookmark ? '#000000' : '#D9D9D9'}` }}
+                  />
+                </div>
+                <Stack spacing={0.5} className='z-0'>
+                  <Stack direction='row' spacing={1.5} alignItems='center'>
+                    <Circle className={`text-sm`} sx={{ color: `${vacancy ? '#76D674' : '#E25454'}` }} />
+                    <Typography gutterBottom variant='h5' component='h2' className='font-Inter'>
+                      {props.data.name}
                     </Typography>
                   </Stack>
+                  <Stack direction='row' spacing={1.5} alignItems='center'>
+                    <PersonOutlineOutlined className='text-xl text-bgNoHover' />
+                    <Stack spacing={-0.5}>
+                      <Typography className='font-Inter text-bgNoHover'>Access available to:</Typography>
+                      <Typography className='font-Inter text-bgNoHover'>
+                        {props.data.accessRights.map(role => (
+                          <ResourceRights key={role} role={role} initialIndex={props.data.accessRights[0]} />
+                        ))}
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                  <Grid container>
+                    {filteredTags.map(tag => (
+                      <ResourceTag key={tag.id} tagData={tag} />
+                    ))}
+                  </Grid>
                 </Stack>
-                <Grid container>
-                  {filteredTags.map(tag => (
-                    <ResourceTag key={tag.id} tagData={tag} />
-                  ))}
-                </Grid>
               </Stack>
-            </Stack>
-          </CardContent>
-        </div>
-      </Card>
-    </Grid>
+            </CardContent>
+          </div>
+        </Card>
+      </Grid>
+      {props.data.type === searchStateMap.SUBSCRIPTIONS && (
+        <SubscriptionModal data={props.data as SubscriptionData} isOpen={openModal} handleClose={handleCloseModal} />
+      )}
+    </>
   );
 };
 
