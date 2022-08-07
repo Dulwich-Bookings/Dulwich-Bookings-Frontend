75cad90010afc65bf8f79dbf73985e9b42a67d9e
Brian Quek <astraxquek@gmail.com>
2022-08-07 17:36:14 +0800

    add MR on subscription modal

--- src/components/Modals/SubscriptionModal/SubscriptionModal.tsx
+++ src/components/Modals/SubscriptionModal/SubscriptionModal.tsx
@@ -13,28 +13,37 @@ type Props = {
 
 const SubscriptionModal = ({ data, handleClose }: Props) => {
   return (
-    <Card className='bg-bgGray rounded-xl w-80 homeLaptop:w-[600px] h-96 hover:shadow-[0_4px_30px_0px_rgba(0,0,0,0.25)]'>
+    <Card className='bg-bgGray rounded-xl w-[355px] homeLaptop:w-[600px] h-96 hover:shadow-[0_4px_30px_0px_rgba(0,0,0,0.25)]'>
       <CardContent className='grow'>
-        <div className='w-full z-10'>
+        <div className='z-10'>
           <CloseIcon className='float-right text-3xl cursor-pointer' onClick={handleClose} sx={{ color: `#404040` }} />
         </div>
-        <Stack spacing={1.5} className='ml-10 m-3' alignItems='start'>
+        <Stack spacing={1.5} className='m-10' alignItems='start'>
           <Stack>
             {data.link && (
-              <a href={data.link} target='_blank' className='font-Inter text-dulwichRed text-[32px]' rel='noreferrer'>
-                <Stack direction='row'>
+              <a
+                href={data.link}
+                target='_blank'
+                className='font-Inter text-dulwichRed text-[24px]  homeLaptop:text-[32px]'
+                rel='noreferrer'
+              >
+                <Stack direction='row' className='items-center'>
                   {data.name}
-                  <OpenInNewIcon className='mt-3.5 ml-1.5' />
+                  <OpenInNewIcon />
                 </Stack>
               </a>
             )}
             {!data.link && <div className='font-Inter text-dulwichRed text-3xl'>{data.name}</div>}
           </Stack>
-          <div className='font-Inter text-sm text-bgBlack mt-0'>{data.description}</div>
+          <div className='font-Inter text-sm text-bgBlack mt-0 w-[235px] homeLaptop:w-[480px] max-h-12 overflow-auto'>
+            {data.description}
+          </div>
 
           <Stack>
             <div className='font-Inter text-bgBlack text-[24px]'>Credentials</div>
-            <div className='font-Inter text-bgBlack text-[20px]'>{textAreaToHTML(data.credentials)}</div>
+            <div className='font-Inter text-bgBlack text-[20px] w-[235px] homeLaptop:w-[480px] max-h-32 overflow-auto'>
+              {textAreaToHTML(data.credentials)}
+            </div>
           </Stack>
         </Stack>
       </CardContent>
