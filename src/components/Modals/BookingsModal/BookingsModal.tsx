b4fa535d4a35403637fad832203363dc27269ef0
AudreyW05 <78786071+AudreyW05@users.noreply.github.com>
2022-08-18 22:14:53 +0800

    put calendar in modal wrapper

--- src/components/Modals/BookingsModal/BookingsModal.tsx
+++ src/components/Modals/BookingsModal/BookingsModal.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 import CloseIcon from '@mui/icons-material/Close';
-import Calendar from '@/components/BookingsModal/Calendar/Calendar';
-import BookingsHeader from '@/components/BookingsModal/BookingsHeader/BookingsHeader';
+import Calendar from '@/components/Modals/BookingsModal/Calendar/Calendar';
+import BookingsHeader from '@/components/Modals/BookingsModal/BookingsHeader/BookingsHeader';
+import ModalWrapper from '@/components/Modals/ModalWrapper/ModalWrapper';
 import { Stack, Modal, Box, ThemeProvider, createTheme } from '@mui/material';
 
 import { UserData } from '@/modules/user/types';
@@ -51,20 +52,24 @@ const BookingsModal = (props: Props) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Modal className='flex justify-center items-center' open={props.openState} onClose={props.handleCloseModal}>
-        <Box className='calendarLaptop:w-11/12 calendarLaptop:h-5/6 calendarLaptop:mt-12 calendarLaptop:rounded-lg w-full h-full px-14 pt-10 bg-white'>
-          <CloseIcon onClick={props.handleCloseModal} className='float-right cursor-pointer hover:text-grayAccent' />
-          <Stack className='h-full' spacing={{ xs: 1, md: -6 }}>
-            <BookingsHeader id={1} resourceData={props.resourceData} />
-            <Calendar
-              resourceData={props.resourceData}
-              currentUser={props.currentUser}
-              currentSchool={props.currentSchool}
-              resourceMaps={resourceMaps}
-            />
-          </Stack>
-        </Box>
-      </Modal>
+      <ModalWrapper
+        isOpen={props.openState}
+        handleClose={props.handleCloseModal}
+        bodyComponent={
+          <Box className='calendarLaptop:w-11/12 calendarLaptop:h-5/6 calendarLaptop:mt-12 calendarLaptop:rounded-lg w-full h-full px-14 pt-10 bg-white'>
+            <CloseIcon onClick={props.handleCloseModal} className='float-right cursor-pointer hover:text-grayAccent' />
+            <Stack className='h-full' spacing={{ xs: 1, md: -6 }}>
+              <BookingsHeader id={1} resourceData={props.resourceData} />
+              <Calendar
+                resourceData={props.resourceData}
+                currentUser={props.currentUser}
+                currentSchool={props.currentSchool}
+                resourceMaps={resourceMaps}
+              />
+            </Stack>
+          </Box>
+        }
+      />
     </ThemeProvider>
   );
 };
