5cdc61419f76b46862f560e8fc2548649cdf8fd2
ShenyiCui <shenyicui@outlook.com>
2023-04-18 05:28:51 +0800

    :sparkles: feat: add bulk signup

--- src/components/Settings/SettingBody/UserDetails/UserDetails.tsx
+++ src/components/Settings/SettingBody/UserDetails/UserDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import SettingHeader from '@/components/Settings/SettingBody/SettingHeader/SettingHeader';
 import { Button, Grid, Stack } from '@mui/material';
@@ -6,6 +6,8 @@ import { UserData } from '@/modules/user/types';
 
 import UserTable from './UserTable/UserTable';
 import TemplateSubmitButton from '@/components/AddResource/Forms/TemplateSubmitButton/TemplateSubmitButton';
+import AuthService from '@/api/auth/AuthService';
+import { useApi } from '@/api/ApiHandler';
 
 type Props = {
   user: UserData;
@@ -14,15 +16,17 @@ type Props = {
 };
 
 const UserDetails = ({ user: userSelf, usersData, handleRefresh }: Props) => {
-  const [placeholderMsg, setPlaceHolderMsg] = useState<string>(''); //Temporary.
+  const [bulkSignUp] = useApi((form: FormData) => AuthService.bulkRegister(form), true, true);
 
   const uploadUserHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
     if (!file) {
       return;
     }
-    const message: string = 'Successfully uploaded ' + file.name;
-    setPlaceHolderMsg(message);
+    const formData = new FormData();
+    formData.append('file', file);
+    await bulkSignUp(formData);
+    window.location.reload();
   };
 
   const downloadTemplateHandler = () => {
@@ -40,12 +44,20 @@ const UserDetails = ({ user: userSelf, usersData, handleRefresh }: Props) => {
             buttonClassName='w-40 h-10 bg-[#808080] rounded-lg text-bgWhite font-inter capitalize'
             buttonTextClassName='text-md'
             buttonText='Upload Users'
-            helperText={placeholderMsg}
+            helperText={''}
             handleOnClick={uploadUserHandler}
           />
-          <Button className='w-48 h-10 bg-[#808080] rounded-lg text-bgWhite capitalize ml-4 text-md' onClick={downloadTemplateHandler}>
+          <a
+            className='w-48 h-10 bg-[#808080] rounded-lg text-bgWhite capitalize ml-4 text-md flex items-center justify-center'
+            href='data:text/csv;charset=utf-8,
+                  exampleStudent@dulwich.org,Student,2000%0A
+                  exampleTeacher@dulwich.com,Teacher,'
+            target='_blank'
+            download='bulk-signup-example.csv'
+            rel='noreferrer'
+          >
             <div className='font-Inter'>Download Template</div>
-          </Button>
+          </a>
         </Grid>
 
         <Grid container className='pt-6'>
